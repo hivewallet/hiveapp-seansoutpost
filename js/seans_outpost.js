6539cce6593c8da813c9d467ce9e22409ad083a2
Taylor Gerring <taylor.gerring@gmail.com>
2014-01-14 11:01:52 -0600

    Respect user preferred Bitcoin unit and use new formatting API

--- js/seans_outpost.js
+++ js/seans_outpost.js
@@ -1,70 +1,55 @@
 /**
  * Created by marcinra on 10/19/13.
  */
-var address = '1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd';
-var separator;
+var donationAddress = '1M72Sfpbz1BPpXFHz9m3CdqATR44Jvaydd';
 var perPersonCostUSD = 1.25;
 var rateBTCUSD = 0.00;
 var userBTC;
+var hiveInfo;
+var defaultAmountSatoshi = 5000000;
 
-$(document).ready(function() {
-    init();
-    $('#in_cash').keyup(function(){ updateEstimate(); });
-});
 
-function isNumber(n) {
-    'use strict';
-    n = n.replace(',', '.');
-    if (!isNaN(parseFloat(n)) && isFinite(n)) {
-        // FIXME remove side-effecting
-        userBTC = n;
-        return true;
-    }
-    return false;
+var init = function(){
+  bitcoin.getSystemInfo(function(info) {
+    hiveInfo = info;
+    $('.preferredBitcoinFormat').text(hiveInfo.preferredBitcoinFormat);
+  });
+
+  $('#in_cash').val(bitcoin.userStringForSatoshi(defaultAmountSatoshi));
+  bitcoin.addExchangeRateListener(setRates);
+  bitcoin.updateExchangeRate('USD');
+
+  $('#in_cash').keyup( updateEstimate );
+  $('#done').click( send );
 }
 
 function updateEstimate() {
-    var amountBTC = $('#in_cash').val();
-    if (isNumber(amountBTC)) {
-        var amountUSD = userBTC * rateBTCUSD;
-        console.log(amountUSD);
-        var peopleFed = amountUSD / perPersonCostUSD;
-        console.log(peopleFed);
-        $('#persons_fed').val(parseInt(peopleFed).toString());
-    } else {
-        $('#persons_fed').val('0');
-    }
+  var userAmountSatoshi = bitcoin.satoshiFromUserString($('#in_cash').val());
+  if ( userAmountSatoshi > 0 ) {
+    var peopleFed = userAmountSatoshi * (rateBTCUSD / bitcoin.BTC_IN_SATOSHI) / perPersonCostUSD;
+    $('#persons_fed').val(parseInt(peopleFed));
+  } else {
+    $('#persons_fed').val(0);
+  }
 }
 
 function setRates(currency, amount) {
-    console.log(currency, amount);
-    if (currency != 'USD')
-        return;
+  if (currency != 'USD')
+    return;
 
-    rateBTCUSD = amount;
-    updateEstimate();
-}
-
-var init = function(){
-    bitcoin.getSystemInfo(function(info) {
-        separator = info.decimalSeparator;
-        var amount_val = document.getElementById('in_cash');
-        amount_val.value = '0'+ separator + '05';
-    });
-    bitcoin.addExchangeRateListener(setRates);
-    bitcoin.updateExchangeRate('USD');
+  rateBTCUSD = amount;
+  updateEstimate();
 }
 
 var success = function(success, transaction_id){
-    if (success){
-        alert("Thanks for donating!");
-    }
+  if (success){
+    alert("Thanks for donating!");
+  }
 }
 
 var send = function(){
-    var amount_val = $('#in_cash').val();
-    var amount = btc_string_to_satoshi(amount_val, separator);
-    if(amount!=0){
-        bitcoin.sendMoney(address, amount, success);
-    }
+  var userAmountSatoshi = bitcoin.satoshiFromUserString($('#in_cash').val());
+  if( userAmountSatoshi > 0 ){
+    bitcoin.sendMoney(donationAddress, userAmountSatoshi, success);
+  }
 }
